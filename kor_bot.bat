cd5c8ab48807c62c696d2f4e49364bd6a47b6d08
frithjof Solterbeck <frithjof.solterbeck@gmail.com>
2026-09-21 18:19:43 +0200

    fixed readme

--- kor_bot.bat
+++ kor_bot.bat
@@ -1,7 +1,6 @@
-@echo off
-REM MailBot. Lagg till flaggor pa sista raden nar du vill att den ska agera:
-REM   --utkast            draftar svar        (ofarligt)
-REM   --utkast --sopa     + slanger brus      (30 dagar i papperskorgen)
-REM   --skarp             + avregistrerar     (gar inte att angra)
-cd /d "%~dp0"
-.venv\Scripts\python.exe listen.py >> bot.out 2>&1
+@echo off
+REM MailBot - slanger brus och sparar utkast. Avregistrering AVSTANGD.
+REM   --utkast  draftar svar      --sopa  slanger brus
+REM   lagg till --avreg for att aven avregistrera (gar inte att angra)
+cd /d "%~dp0"
+.venv\Scripts\python.exe listen.py --utkast --sopa >> bot.out 2>&1
